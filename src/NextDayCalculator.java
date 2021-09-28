040230bd5ab2124841be95f1b205ec745a3a8d0f
batuanr <tuanpesi@gmail.com>
2021-09-28 11:41:42 +0700

    refactor

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,11 +3,14 @@ public class NextDayCalculator {
     public static final String CONCATENATION = "/";
 
     public static String getNextDay (int day, int month, int year) {
-        if (day == 31) {
-            day =1;
+        int lastOfMonth = 31;
+        if (day == lastOfMonth) {
+            int startOfMonth = 1;
+            day = startOfMonth;
             month++;
         }
         else {
+
             day++;
         }
         return day + CONCATENATION + month + CONCATENATION + year;
